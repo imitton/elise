4a7e0afbe3803f09f98f319a8cf6423f0fe550a5
imitton <53008934+imitton@users.noreply.github.com>
2022-06-23 16:22:00 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   base_url: 'https://imitton.github.io/elise/#/home',
-  cases_json_url: 'https://raw.githubusercontent.com/GeoTecINIT/elise/main/src/assets/cases.json'
+  cases_json_url: 'https://raw.githubusercontent.com/imitton/elise/main/src/assets/cases.json'
 };
